a7503f4342129f45a1760a1b2296ab23466b32a5
krishna <krishnarao4829@gmail.com>
2020-07-22 17:07:23 +0530

    Updates

---
+++ precache-manifest.46a6a62799e095fc2604ecc7d5397ff2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7cdbf9d6d67d9a69afcc8f9356da718",
+    "url": "/BookList/index.html"
+  },
+  {
+    "revision": "23692958091ec8b76855",
+    "url": "/BookList/static/css/main.f3c24ab7.chunk.css"
+  },
+  {
+    "revision": "fa37f865dbfa8a53eb95",
+    "url": "/BookList/static/js/2.f1b28e9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BookList/static/js/2.f1b28e9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23692958091ec8b76855",
+    "url": "/BookList/static/js/main.f37c79b4.chunk.js"
+  },
+  {
+    "revision": "6801ac5c186d632b29f5",
+    "url": "/BookList/static/js/runtime-main.d48b8508.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Booklist/precache-manifest.1f6566e62ceb58beb8120b961064c641.js"
+  "/BookList/precache-manifest.46a6a62799e095fc2604ecc7d5397ff2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Booklist/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BookList/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
