d503b24abf4a452b3549e4efa6d33bf13d639f24
krishna <krishnarao4829@gmail.com>
2020-07-22 16:41:59 +0530

    Updates

---
+++ precache-manifest.1f6566e62ceb58beb8120b961064c641.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9506a2df4a5fe52f59bbcab672ca3006",
+    "url": "/Booklist/index.html"
+  },
+  {
+    "revision": "478debebfbf917a60598",
+    "url": "/Booklist/static/css/main.f3c24ab7.chunk.css"
+  },
+  {
+    "revision": "fa37f865dbfa8a53eb95",
+    "url": "/Booklist/static/js/2.f1b28e9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Booklist/static/js/2.f1b28e9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "478debebfbf917a60598",
+    "url": "/Booklist/static/js/main.927c5fd3.chunk.js"
+  },
+  {
+    "revision": "49e8bc5c137f38357feb",
+    "url": "/Booklist/static/js/runtime-main.82ec8f53.js"
+  }
+]);
